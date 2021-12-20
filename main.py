fe2eb76d930814ae298a11226eeeab91850cd6c3
unknown <arek91w@gmail.com>
2021-12-20 18:06:13 +0100

    ver 2

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ def preprocess_image(image):
 
     # ladowanie zdjecia
     im = cv2.imread(image)
+    imag = cv2.imread(image)
 
     # konwersja na zdjecie czarno-biale oraz dadanie rozmycia Gaussa
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
@@ -64,15 +65,15 @@ def preprocess_image(image):
     cv2.circle(im, extRight, 8, (0, 255, 0), -1)
     cv2.circle(im, extTop, 8, (255, 0, 0), -1)
     cv2.circle(im, extBot, 8, (255, 255, 0), -1)
-
-    cv2.putText(im, 'A', (100,100), font, 3, (0, 255, 0), 2, cv2.LINE_AA)
     # show the output image
     cv2.imshow(image, im)
 
     cv2.waitKey(0)
 
-    return extLeft, extRight, extTop, extBot
+    return imag, extLeft, extRight, extTop, extBot
 
+
+# funkcja obliczajaca charakterystyki pukntow charakterystycznych
 
 def predict(left, right, top, bot):
     width = right[0] - left[0]
@@ -81,21 +82,25 @@ def predict(left, right, top, bot):
     y_shift = top[0] - left[0]
     return width, heigth, x_shift, y_shift
 
-    print("predict")
-
-l, r, t, b = preprocess_image(f"images/Y3.jpg")
+imag, l, r, t, b = preprocess_image(f"images/V3.jpg")
 
 width, heigth, x_shift, y_shift = predict(l, r, t, b)
 
+
+# na podstawie charekterystyk przewidywanie znaku
+
 if width < 430 and width > 394 and heigth < 560 and heigth > 502 and x_shift < -120 and x_shift > -150 and y_shift < 366 and y_shift > 320:
-    print("A")
+    cv2.putText(imag, 'A', (100,100), font, 3, (0, 255, 0), 2, cv2.LINE_AA)
 elif width < 340 and width > 294 and heigth < 710 and heigth > 600 and x_shift < 20 and x_shift > -66 and y_shift < 200 and y_shift > 144:
-    print("B")
+    cv2.putText(imag, 'B', (100,100), font, 3, (0, 255, 0), 2, cv2.LINE_AA)
 elif width < 582 and width > 520 and heigth < 710 and heigth > 660 and x_shift < -110 and x_shift > -170 and y_shift < 300 and y_shift > 244:
-    print("L")
+    cv2.putText(imag, 'L', (100,100), font, 3, (0, 255, 0), 2, cv2.LINE_AA)
 elif width < 350 and width > 290 and heigth < 674 and heigth > 580 and x_shift < -70 and x_shift > -200 and y_shift < 130 and y_shift > 80:
-    print("V")
+    cv2.putText(imag, 'V', (100,100), font, 3, (0, 255, 0), 2, cv2.LINE_AA)
 elif width < 600 and width > 560 and heigth < 640 and heigth > 600 and x_shift < 240 and x_shift > 200 and y_shift < 30 and y_shift > 0:
-    print("Y")
+    cv2.putText(imag, 'Y', (100,100), font, 3, (0, 255, 0), 2, cv2.LINE_AA)
 else:
-    print("ni mozno rozpoznac")
+    print("can't recognise")
+
+cv2.imshow("PREDICT", imag)
+cv2.waitKey(0)
